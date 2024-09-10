76b4461b9c629356ae333d4baf5ebbbd0502c0e0
rosafish <rosazhou1997@gmail.com>
2024-09-10 13:15:54 -0500

    add data and shell scripts

---
+++ shell_scripts/esnli_k16_flame_phTrue_davinci_predict-then-explain.sh
@@ -0,0 +1,49 @@
+PET_DIR=/data/rosa/flame #TODO: point this to your flame directory
+
+DATA_DIR=../data/e-SNLI-k16 #TODO: point this to your data directory
+MODEL_TYPE=roberta
+MODEL=roberta-large
+TASK=esnli
+
+# -1 => Use everything\n\
+NUM_TRAIN=-1
+NUM_TEST=-1
+NUM_UNLABEL=-1
+
+export CUDA_VISIBLE_DEVICES=1
+
+MSG=esnli_k16_flame_phTrue_davinci_predict-then-explain
+python3 $PET_DIR/cli.py \
+--method pet \
+--pattern_ids 0 1 2 3 \
+--data_dir $DATA_DIR \
+--model_type $MODEL_TYPE \
+--model_name_or_path $MODEL \
+--task_name $TASK \
+--output_dir $PET_DIR/outputs/$TASK/pet_$TASK_$MSG \
+--do_train \
+--do_eval \
+--overwrite_output_dir \
+--cache_dir $PET_DIR/cache/$TASK/$MODEL/ \
+--train_examples $NUM_TRAIN \
+--test_examples $NUM_TEST \
+--unlabeled_examples $NUM_UNLABEL \
+--pet_max_steps 1000 \
+--pet_per_gpu_train_batch_size 1 \
+--pet_per_gpu_unlabeled_batch_size 3 \
+--pet_gradient_accumulation_steps 4 \
+--no_distillation \
+--eval_result $MSG \
+--wandb_run_name pet_$TASK_$MSG \
+--train_custom_expl_file ${DATA_DIR}/phle_expl/train_davinci_phle.jsonl \
+--dev_custom_expl_file ${DATA_DIR}/phle_expl/dev_davinci_phle.jsonl \
+--test_custom_expl_file ${DATA_DIR}/phle_expl/test_davinci_phle.jsonl \
+--eval_with_three_labels_explanations_logits \
+--e_pet_pred \
+--e_pet_test \
+--train_gold_expl \
+--train_with_three_labels_explanations \
+--calibration \
+--beta_requires_grad \
+--beta 0.5 \
+--beta_lr 2e-3
---
+++ shell_scripts/esnli_k16_pet_no-expl.sh
@@ -0,0 +1,38 @@
+PET_DIR=/data/rosa/flame #TODO: point this to your flame directory
+
+DATA_DIR=../data/e-SNLI-k16 #TODO: point this to your data directory
+MODEL_TYPE=roberta
+MODEL=roberta-large
+TASK=esnli
+
+# -1 => Use everything
+NUM_TRAIN=-1
+NUM_TEST=-1
+NUM_UNLABEL=-1
+
+export CUDA_VISIBLE_DEVICES=0
+
+MSG=esnli_k16_pet_no_expl
+python3 $PET_DIR/cli.py \
+--method pet \
+--data_dir $DATA_DIR \
+--model_type $MODEL_TYPE \
+--model_name_or_path $MODEL \
+--task_name $TASK \
+--output_dir $PET_DIR/outputs/$TASK/pet/$MSG \
+--do_train \
+--do_eval \
+--overwrite_output_dir \
+--cache_dir $PET_DIR/cache/$TASK/$MODEL/ \
+--train_examples $NUM_TRAIN \
+--test_examples $NUM_TEST \
+--unlabeled_examples $NUM_UNLABEL \
+--no_distillation \
+--eval_result $MSG \
+--wandb_run_name pet_$TASK_$MSG \
+--save_train_logits \
+--pattern_ids 0 1 2 3 \
+--pet_max_steps 1000 \
+--pet_per_gpu_train_batch_size 1 \
+--pet_per_gpu_unlabeled_batch_size 3 \
+--pet_gradient_accumulation_steps 4 
